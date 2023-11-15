da70ca7762177ddccd65fd543f4c6f998e05613c
AmanPachori <amangirrajpachori2002@gmail.com>
2023-11-15 11:41:19 +0530

    Favourites Listing page added

---
+++ app/favorites/FavouritesClient.tsx
@@ -0,0 +1,45 @@
+import { Listing, User } from "@prisma/client";
+import React from "react";
+import Heading from "../components/atoms/Heading/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/molecules/listings/ListingCard";
+import { SafeListing } from "../types";
+
+interface FavoritesClientProps {
+  listings: SafeListing[];
+  currentUser: User;
+}
+
+const FavoritesClient: React.FC<FavoritesClientProps> = ({
+  listings,
+  currentUser,
+}) => {
+  return (
+    <Container>
+      <Heading title="Favorites" subtitle="List of places you favorited!" />
+      <div
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default FavoritesClient;
